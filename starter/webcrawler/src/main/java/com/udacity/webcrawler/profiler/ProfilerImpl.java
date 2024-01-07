fbcbe3dc3bba3c1feea293a97042d36f11b7ae39
cong dinh <congdinh2012@hotmail.com>
2024-01-08 00:16:07 +0700

    Implementing the Profiler

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
@@ -1,12 +1,16 @@
 package com.udacity.webcrawler.profiler;
 
 import javax.inject.Inject;
+
 import java.io.IOException;
 import java.io.Writer;
 import java.nio.file.Path;
+import java.nio.file.Files;
 import java.time.Clock;
 import java.time.ZonedDateTime;
-import java.util.Objects;
+import java.lang.reflect.Proxy;
+import java.lang.reflect.Method;
+import java.util.*;
 
 import static java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME;
 
@@ -29,17 +33,32 @@ final class ProfilerImpl implements Profiler {
   public <T> T wrap(Class<T> klass, T delegate) {
     Objects.requireNonNull(klass);
 
-    // TODO: Use a dynamic proxy (java.lang.reflect.Proxy) to "wrap" the delegate in a
+    // Use a dynamic proxy (java.lang.reflect.Proxy) to "wrap" the delegate in a
     //       ProfilingMethodInterceptor and return a dynamic proxy from this method.
     //       See https://docs.oracle.com/javase/10/docs/api/java/lang/reflect/Proxy.html.
+    List<Method> methods = Arrays.asList(klass.getMethods());
 
-    return delegate;
+    if (methods.isEmpty()) {
+      throw new IllegalArgumentException(klass.getName() + " has not methods.");
+    }
+
+    ProfilingMethodInterceptor pmi = new ProfilingMethodInterceptor(clock, state, delegate);
+
+    return (T) Proxy.newProxyInstance(ProfilerImpl.class.getClassLoader(), new Class[] {klass}, pmi);
   }
 
   @Override
-  public void writeData(Path path) {
-    // TODO: Write the ProfilingState data to the given file path. If a file already exists at that
+  public void writeData(Path path) throws IOException {
+    // Write the ProfilingState data to the given file path. If a file already exists at that
     //       path, the new data should be appended to the existing file.
+    Objects.requireNonNull(path);
+
+    try (Writer writer = Files.newBufferedWriter(path)) {
+      writeData(writer);
+      writer.flush();
+    } catch (IOException ex) {
+      throw new IOException("Failed to write data to path: " + path, ex);
+    }
   }
 
   @Override
